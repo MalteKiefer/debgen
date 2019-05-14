133e5971b6938fd980387cbcaa264fe8a698ddff
Malte Kiefer <malte.kiefer@mailgermania.de>
2019-05-14 10:35:46 +0200

    dev

--- tpl/footer.php
+++
@@ -1,8 +0,0 @@
-
-			Thank you for using our service!<br /><br /><br />
-			</div>
-		</main>
-		<script src="https://stats.simplylinux.ch/piwik.js"></script>
-		<script src="js/piwik.js"></script>
-	</body>
-</html>
