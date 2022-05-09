aa9211513951018bccebca63cb3886de1a74793b
Malte Kiefer <malte.kiefer@intellytec.de>
2022-05-09 02:23:40 +0200

    filter

--- src/components/SourceGenerator.vue
+++ src/components/SourceGenerator.vue
@@ -8,12 +8,20 @@ export default {
   data: () => ({
     repos: [],
     categories: [],
+    releases: [],
   }),
   methods: {
     getRepos: function () {
       this.$http.get('https://raw.githubusercontent.com/MalteKiefer/debgen/master/repos.json')
           .then(function (response) {
-            console.log(response.data)
+            this.repos = response.data
+            this.categories = response.data.map(({ category }) => category)
+            this.categories = [...new Set(this.categories)];
+            this.releases = response.data.map(({ release }) => release)
+            this.releases = [...new Set(this.releases)];
+
+            console.log(this.categories)
+
           }.bind(this))
     },
   },
